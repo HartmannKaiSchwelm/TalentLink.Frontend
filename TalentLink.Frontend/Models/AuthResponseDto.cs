a18dfcf6f039c8e127a964f7785b7c1ae5403346
Kai <Pappa@DESKTOP-29NAR0C>
2025-05-21 09:33:12 +0200

    added basic loginPage, function needs to be added, created AuthResponseDto and LoginDto

---
+++ TalentLink.Frontend/Models/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TalentLink.Frontend.Models
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
